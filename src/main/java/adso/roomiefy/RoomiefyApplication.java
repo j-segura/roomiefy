14fcc2194d189803047664a2f4bd3756c2a5da4d
Juan David Segura Zabala <juanda.segura2005@gmail.com>
2024-02-11 17:37:42 -0500

    'db'

---
+++ src/main/java/adso/roomiefy/RoomiefyApplication.java
@@ -0,0 +1,13 @@
+package adso.roomiefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiefyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adso/roomiefy/RoomiefyApplicationTests.java
@@ -0,0 +1,13 @@
+package adso.roomiefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
